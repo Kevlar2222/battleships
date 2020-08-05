49f5320c25696d622e02ba0687abc16aa2a1a071
Kevlar2222 <kevin.satti@hotmail.co.uk>
2020-08-05 17:32:06 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/battleships/precache-manifest.c5bc8dea3465dd455c550f30840d1ad1.js"
+  "/battleships/precache-manifest.5a3b88ae99f3faa57df0aa8a0ff89e7f.js"
 );
 
 self.addEventListener('message', (event) => {
